ec23cd2b8cfa809e0aa2f806686319f8cb78532d
danilromas <furiblack904@gmail.com>
2022-11-22 16:44:20 +0300

    firts pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home_page', function(req, res, next) {
+  res.render("<h1>Страница Основная</h1>");
+});
+
+/* Обьяснение что это такое */
+router.get('/about', function(req, res, next) {
+  res.render("<h1>Обьяснение</h1>");
+});
+
+/* GET home page. */
+router.get('/characters', function(req, res, next) {
+  res.render("<h1>Персонажи</h1>");
 });
 
 module.exports = router;
