0a1419c7f75db739bb3e3f669f52f04c76ad68c3
danilromas <furiblack904@gmail.com>
2022-11-22 18:20:22 +0300

    4_3

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,41 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/home_page', function(req, res, next) {
-  res.render("<h1>Страница Основная</h1>");
+  res.send("<h1>Страница Основная</h1>");
 });
 
 /* Обьяснение что это такое */
-router.get('/about', function(req, res, next) {
-  res.render("<h1>Обьяснение</h1>");
+router.get('/about1', function(req, res, next) {
+  res.send("<h1>Обьяснение</h1>");
 });
 
 /* GET home page. */
-router.get('/characters', function(req, res, next) {
-  res.render("<h1>Персонажи</h1>");
+router.get('/characters1', function(req, res, next) {
+  res.send("<h1>Персонажи</h1>");
 });
+
+router.get('/index', function(req, res, next) { 
+ res.render('index', {
+          title: "Карамелька", 
+          picture: "images/karamelka.jpg",   
+          desc: "проверка"
+        });
+      });
+
+router.get('/about', function(req, res, next) { 
+        res.render('about', {
+                 title: "Здесь инфа об психологии", 
+                 picture: "images/karamelka.jpg",   
+                 desc: "проверка"
+               });
+             });
+             
+router.get('/characters', function(req, res, next) { 
+              res.render('characters16', {
+                       title: "о персонажах психологии", 
+                       picture: "images/karamelka.jpg",   
+                       desc: "проверка"
+                     });
+                   });
 
 module.exports = router;
