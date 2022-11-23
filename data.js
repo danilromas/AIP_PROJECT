628f781bdef96810eae862ce4a86f228996acc40
danilromas <furiblack904@gmail.com>
2022-11-23 16:41:46 +0300

    Module

---
+++ data.js
@@ -0,0 +1,23 @@
+var data = [
+    {
+    title: 'Посредник',
+    nick: 'INFP',
+    avatar: '/images/infp.jpeg',
+    desc: 'Посредник'
+    },
+    {
+    title: 'Активист',
+    nick: 'INFJ',
+    avatar: '/images/INFJ.jpeg',
+    desc: 'Активист'
+    },
+    {
+    title: 'Непридумал',
+    nick: 'crypto',
+    avatar: '/images/nono.png',
+    desc: 'ЗАписать сюда еще кого-то'
+    }
+];
+
+
+module.exports.data = data;
