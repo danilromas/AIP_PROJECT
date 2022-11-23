e092f3c5cd5d1e6a32a032a976deb8560d082448
danilromas <furiblack904@gmail.com>
2022-11-23 16:22:02 +0300

    CreateDB

---
+++ createDB.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        await client.connect();
+        var database = client.db("apexlegends");
+        database.dropDatabase();
+        database = client.db("apexlegends");
+        const legends = database.collection("legends");
+        const result = await legends.insertOne({name:"Рэйф"});
+        console.log(`${result} documents were inserted`);
+    }
+    finally {
+        await client.close();
+    }
+}
+run()
