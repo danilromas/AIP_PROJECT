0b678467f0377757744ccc4bfc3ddbc22b67b236
danilromas <furiblack904@gmail.com>
2022-12-07 17:59:53 +0300

    Routerr

--- routes/16char.js
+++ routes/16char.js
@@ -3,12 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с legends');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с char');
 });
 
 /* Страница харакетров */
 router.get('/:nick', function(req, res, next) {
-    res.send(req.params.nick);
+    char.findOne({nick:req.params.nick}, function(err, char) {
+        if (err) return next(err)
+        if (!char) return next(new Error ("Нет такой легенды"))
+        res.render('char', {
+          title: char.title,
+          picture: char.avatar,
+          desc: char.desc
+        })
+      })
 });
 
 module.exports = router;
