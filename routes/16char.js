0ccd493f2aed85bb08713141aa826076f7aaa865
danilromas <furiblack904@gmail.com>
2022-12-21 20:32:47 +0300

    исправление ошибок

--- routes/16char.js
+++ routes/16char.js
@@ -15,4 +15,17 @@ router.get('/:nick', function(req, res, next) {
       })
 });
 
+/* Страница интелекта */
+router.get('/:nick', function(req, res, next) {
+  char.findOne({nick:req.params.nick}, function(err, char) {
+      if (err) return next(err)
+      if (!char) return next(new Error ("Нет такой легенды"))
+      res.render('char', {
+        title: char.title,
+        picture: char.avatar,
+        desc: char.desc
+      })
+    })
+});
+
 module.exports = router;
